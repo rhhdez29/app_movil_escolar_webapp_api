3c62a66f3404e79654b0351b7edd166086060be1
rhhdez29 <rafaelhdez290@gmail.com>
2025-10-14 22:15:06 -0600

    primer commit

---
+++ app_movil_escolar_webapp_api/urls.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from .views.bootstrap import VersionView
+from app_movil_escolar_webapp_api.views import bootstrap
+from app_movil_escolar_webapp_api.views import users
+from app_movil_escolar_webapp_api.views import auth
+#from app_movil_escolar_webapp_api.views import alumnos
+#from app_movil_escolar_webapp_api.views import profesores
+
+urlpatterns = [
+    
+    #Create admin
+    path('admin/', users.AdminView.as_view()),
+    #Admin data
+    path('list/admin', users.AdminAll.as_view()),
+    #Edit admin
+    #path('admins-edit/', users.AdminsViewEdit.as_view()),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
