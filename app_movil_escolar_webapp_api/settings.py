9f10077f55b9930b2b68d7842b8470464976cc3b
rhhdez29 <rafaelhdez290@gmail.com>
2025-11-29 14:56:19 -0600

    correcion settings

--- app_movil_escolar_webapp_api/settings.py
+++ app_movil_escolar_webapp_api/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
         default=os.environ.get("DATABASE_URL"),
         conn_max_age=600,
         conn_health_checks=True,
-    )
+    ),
     'default2': {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
